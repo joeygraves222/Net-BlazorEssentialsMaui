e5e14011eedbb3043779f92fa110fc4ffd7d5a1a
Joseph Graves <jgraves@eforcesoftware.com>
2022-08-22 11:43:21 -0600

    Add project files.

---
+++ Net-BlazorEssentialsMaui/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BlazorEssentials.Models;
+using BlazorEssentials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Net_BlazorEssentialsMaui.Services;
+
+namespace Net_BlazorEssentialsMaui.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddBlazorEssentialsMaui(this IServiceCollection services, string BaseURL)
+        {
+            services.AddScoped<IStorageManager, StorageManager>();
+            services.AddScoped<ILocationService, LocationServiceMaui>();
+            services.AddScoped<Interop>();
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(BaseURL) });
+            services.AddScoped<IAuthService, AuthService>();
+
+            return services;
+        }
+    }
+}
---
+++ Net-BlazorEssentialsMaui/Services/LocationService.cs
@@ -0,0 +1,36 @@
+﻿using BlazorEssentials.Models;
+using Microsoft.Maui.Devices.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_BlazorEssentialsMaui.Services
+{
+    public class LocationServiceMaui : ILocationService
+    {
+        public async Task<GeoLocation> GetLocationAsync()
+        {
+            try
+            {
+                Location location = await Geolocation.GetLocationAsync();
+
+                
+                return new GeoLocation() { Lat = location.Latitude, Lon = location.Longitude };
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    Location loc = await Geolocation.GetLastKnownLocationAsync();
+                    return new GeoLocation() { Lat = loc.Latitude, Lon = loc.Longitude };
+                }
+                catch (Exception exc)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
